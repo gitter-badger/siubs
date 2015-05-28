b64c9a72844fea5a6af0934fe85864675fe91157
Paulo Tada <paulohtfs@gmail.com>
2015-05-28 19:20:32 -0300

    Adding more 2 tests to model district

--- spec/models/district_spec.rb
+++ spec/models/district_spec.rb
@@ -7,35 +7,44 @@ RSpec.describe District, type: :model do
 	end
 
 	# Valid attributes for creating a District
-	let(:valid_attr){{district_name: "CENTRO"}}
+	let(:valid_attr_name){{district_name: "CENTRO"}}
+	let(:valid_attr_name_number){{district_name: "CENTRO 1"}}
 	let(:invalid_attr_name_blank){{district_name: ""}}
 	let(:invalid_attr_name_number){{district_name: "12314"}}
 	let(:invalid_attr_name_char){{district_name: "@!#{!$!}"}}
 
 	# Describes district unit test
-	describe District do
+	describe "Validation" do
 		context "when enter valid attributes" do
-			subject(:valid_district){District.new(valid_attr)}
-			it "should set a district" do
-				expect(valid_district.valid?).to be_truthy
+			context "only letters" do
+				subject(:valid_district){District.new(valid_attr_name)}
+				it "should set a district" do
+					expect(valid_district.valid?).to be_truthy
+				end
+			end
+			context "letters and numbers" do
+				subject(:valid_district){District.new(valid_attr_name_number)}
+				it "should set a district" do
+					expect(valid_district.valid?).to be_truthy
+				end
 			end
 		end
 
 		context "when enter invalid attributes" do
 
-			describe "blank name" do
+			context "blank name" do
 				subject(:invalid_district){District.new(invalid_attr_name_blank)}
 				it "should not be valid" do
 					expect(invalid_district.valid?).to be_falsey
 				end
 			end
-			describe "with just numbers" do
+			context "with just numbers" do
 				subject(:invalid_district){District.new(invalid_attr_name_number)}
 				it "should not be valid" do
 					expect(invalid_district.valid?).to be_falsey
 				end
 			end
-			describe "with just special chars" do
+			context "with just special chars" do
 				subject(:invalid_district){District.new(invalid_attr_name_char)}
 				it "should not be valid" do
 					expect(invalid_district.valid?).to be_falsey
