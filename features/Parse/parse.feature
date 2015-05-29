a15990e162f45281553b456d6ff01c90480c86b0
Maria Luciene <marialucienefelix@gmail.com>
2015-05-29 00:34:40 -0300

    Adding more steps to parse acceptance test

--- features/Parse/parse.feature
+++ features/Parse/parse.feature
@@ -3,7 +3,9 @@ Feature: Parse
   As a developer
   I want to upload a file
 
-  Scenario: Uploading a file
-    Given I am on the parse page
-    When I press importar button
-    Then The file "ubs.csv" sould exits
+  Scenario: Uploading a valid file
+    Given I am on the new parse page
+    When I upload a csv file
+    And I press importar button
+    Then the number of ubs should change
+    And I should see the file upload page again
--- features/step_definitions/parse_step.rb
+++ features/step_definitions/parse_step.rb
@@ -1,13 +1,21 @@
 
-Given(/^I am on the parse page$/) do
-	visit('/parses')
+Given(/^I am on the new parse page$/) do
+	@ubs_amount = Ubs.count 
+	visit('/parses/new')
+end
+
+When(/^I upload a csv file$/) do
+	attach_file(:file, File.join(Rails.root, 'public', 'ubs2.csv'))
 end
 
 When(/^I press importar button$/) do
-	visit('/parses/new')
 	click_button('importar')
 end
 
-Then(/^The file "([^"]*)" sould exits$/) do |file|
-  
+Then(/^the number of ubs should change$/) do
+	Ubs.count.should > @ubs_amount
+end
+
+Then(/^I should see the file upload page again$/) do
+	visit('/parses/new')
 end
