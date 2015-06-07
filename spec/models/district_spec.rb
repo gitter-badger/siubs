15a822877423493df82b23f8f2be655a0f9225db
Paulo Tada <paulohtfs@gmail.com>
2015-06-07 11:42:07 -0300

    Adding test for user model and refactoring district

--- spec/models/district_spec.rb
+++ spec/models/district_spec.rb
@@ -9,44 +9,46 @@ RSpec.describe District, type: :model do
 	# Valid attributes for creating a District
 	let(:valid_attr_name){{district_name: "CENTRO"}}
 	let(:valid_attr_name_number){{district_name: "CENTRO 1"}}
-	let(:invalid_attr_name_blank){{district_name: ""}}
-	let(:invalid_attr_name_number){{district_name: "12314"}}
-	let(:invalid_attr_name_char){{district_name: "!@#$%*{}"}}
+	let(:invalid_attr_blank_name){{district_name: ""}}
+	let(:invalid_attr_number_name){{district_name: "12314"}}
+	let(:invalid_attr_char_name){{district_name: "!@#$%*{}"}}
 
 	# Describes district unit test
 	describe "Validation" do
 		context "when enter valid attributes" do
+
+			subject(:valid_district_name){District.new(valid_attr_name)}
+			subject(:valid_district_name_number){District.new(valid_attr_name_number)}
 			context "only letters" do
-				subject(:valid_district){District.new(valid_attr_name)}
 				it "should create a district" do
-					expect(valid_district.valid?).to be_truthy
+					expect(valid_district_name.valid?).to be_truthy
 				end
 			end
 			context "letters and numbers" do
-				subject(:valid_district){District.new(valid_attr_name_number)}
 				it "should create a district" do
-					expect(valid_district.valid?).to be_truthy
+					expect(valid_district_name_number.valid?).to be_truthy
 				end
 			end
 		end
 
 		context "when enter invalid attributes" do
+			subject(:invalid_district_blank_name){District.new(invalid_attr_blank_name)}
+			subject(:invalid_district_number_name){District.new(invalid_attr_number_name)}
+			subject(:invalid_district_char_name){District.new(invalid_attr_char_name)}
 			context "blank name" do
-				subject(:invalid_district){District.new(invalid_attr_name_blank)}
 				it "should not be created" do
-					expect(invalid_district.valid?).to be_falsey
+					expect(invalid_district_blank_name.valid?).to be_falsey
 				end
 			end
 			context "with just numbers" do
-				subject(:invalid_district){District.new(invalid_attr_name_number)}
 				it "should not be created" do
-					expect(invalid_district.valid?).to be_falsey
+					expect(invalid_district_number_name.valid?).to be_falsey
 				end
 			end
 			context "with just special chars" do
-				subject(:invalid_district){District.new(invalid_attr_name_char)}
+				
 				it "should not be created" do
-					expect(invalid_district.valid?).to be_falsey
+					expect(invalid_district_char_name.valid?).to be_falsey
 				end
 			end
 		end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,74 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	# Valid attributes for user
+	let(:valid_attr){{
+		user_name: "PAULO",
+		email: "paulohtfs@gmail.com",
+		password:"mudar123"
+	}}
+	let(:valid_attr_diff){{
+		user_name: "PAULO123",
+		email: "paulohtfs_99@gmail.com",
+		password:"mudar123"
+	}}
+
+	# Invalid attributes for user
+	let(:invalid_attr_email){{
+		user_name: "PAULO",
+		email: "email",
+		password:"mudar123"
+	}}
+	let(:invalid_attr_duplicate_name){{
+		user_name: "PAULO",
+		email: "paulohtfs_1100@gmail.com",
+		password:"mudar123"
+	}}
+	let(:invalid_attr_duplicate_email){{
+		user_name: "PAULO332",
+		email: "paulohtfs@gmail.com",
+		password:"mudar123"
+	}}
+	let(:invalid_attr_passw_less_then_8){{
+		user_name: "PAULO",
+		email: "paulohtfs555@gmail.com",
+		password:"mudar12"
+	}}
+
+	# REVIEW: thats the best way for testing a model? by using "create!" method, or use "new"
+	context "Validation" do
+
+		context "when enter valid attributes" do
+			subject(:valid_user){User.create!(valid_attr)}
+			subject(:valid_user_diff){User.create!(valid_attr_diff)}
+			it "should create a user" do
+				expect(valid_user.valid?).to be_truthy
+			end
+			it "should create a second user" do
+				expect(valid_user_diff.valid?).to be_truthy
+			end
+		end
+
+		context "when enter invalid attributes" do
+			subject(:erro_msg_email){"Validation failed: Email is invalid"}
+			subject(:erro_msg_email_dpl){"Validation failed: Email has already been taken"}
+			subject(:erro_msg_passwd){"Validation failed: Password is too short (minimum is 8 characters)"}
+			it "should not create a user with invalid email" do
+				expect{User.create!(invalid_attr_email)}.to raise_error(ActiveRecord::RecordInvalid, erro_msg_email)
+			end
+			# TODO: Raise a appropriate exception
+			it "should not create a user with duplicated name" do
+				User.create!(valid_attr)
+				expect{User.create!(invalid_attr_duplicate_name)}.to raise_error(ActiveRecord::RecordInvalid)
+			end
+			it "should not create a user with duplicated email" do
+				User.create!(valid_attr)
+				expect{User.create!(invalid_attr_duplicate_email)}.to raise_error(ActiveRecord::RecordInvalid, erro_msg_email_dpl)
+			end
+			it "should not create a user with less then 8 char password" do
+				expect{User.create!(invalid_attr_passw_less_then_8)}.to raise_error(ActiveRecord::RecordInvalid, erro_msg_passwd)
+			end
+		end
+	end
 end
