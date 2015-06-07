480544fc1d34311de3c037aba42bae99b4d433ef
Paulo Tada <paulohtfs@gmail.com>
2015-06-07 18:15:44 -0300

    Updating travis and fixing parser cucumber test

--- features/step_definitions/parse_step.rb
+++ features/step_definitions/parse_step.rb
@@ -1,10 +1,12 @@
+CSV_DIR = "public/csv"
+CSV_FILE = "ubs_test.csv"
 
 Given(/^I am on the new parse page$/) do
 	visit('/parses/new')
 end
 
 When(/^I upload a csv file$/) do
-	attach_file(:file, File.join(Rails.root, 'public', 'ubs2.csv'))
+	attach_file(:file, File.join(Rails.root, CSV_DIR, CSV_FILE))
 end
 
 When(/^I press importar button$/) do
@@ -19,7 +21,7 @@ Then(/^I should see the file upload page again$/) do
 end
 
 When(/^I upload a different extension file$/) do
-   attach_file(:file, File.join(Rails.root, 'public', 'ubs2.doc'))	
+   attach_file(:file, File.join(Rails.root, CSV_DIR, CSV_FILE))	
 end
 
 Then(/^the ubs was not created$/) do
