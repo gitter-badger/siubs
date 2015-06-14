03915c88094f60a62563e79e88132399789cf971
Paulo Tada <paulohtfs@gmail.com>
2015-06-14 13:55:12 -0300

    Adding attribute "active" to users [isnt using yet]

--- db/migrate/20150607014743_create_users.rb
+++ db/migrate/20150607014743_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
     	t.string :user_name, null: false
+    	t.boolean :user_active, default: true
 
     	t.timestamps null: false
     end
