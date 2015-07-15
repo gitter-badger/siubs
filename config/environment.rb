b9c3f4261e9460b05b761a7944a4514546a3ac53
Paulo Tada <paulohtfs@gmail.com>
2015-07-14 23:36:09 -0300

    Fixing coveralls

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,9 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
 # Initialize coveralls on Rails application.
 require 'coveralls'
 Coveralls.wear!('rails')
-
-# Load the Rails application.
-require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
